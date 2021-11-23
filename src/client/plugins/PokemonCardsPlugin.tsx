1fb1f06538abde68fc7396588d8290d86cf69e17
t1pp1 <pengzouyiyun@gmail.com>
2021-11-22 21:19:51 -0500

    fix: move the visualization plugin

--- src/client/plugins/PokemonCardsPlugin.tsx
+++ src/client/plugins/PokemonCardsPlugin.tsx
@@ -1,35 +1,19 @@
 import * as React from 'react';
 import { useState, useEffect, useContext } from 'react';
 import PokemonCardsPage from '../components/PokemonCardsPage';
-import { PokemonClient } from "pokenode-ts";
 import { Context } from "../components/Store";
 
 
 const PokemonCardsPlugin = () => {
-	const [greeting, setGreeting] = useState<string>('');
 	const [state, dispatch] = useContext(Context);
 
 	useEffect(() => {
-		async function getGreeting() {
-			try {
-				const res = await fetch('/api/hello');
-				const greeting = await res.json();
-				setGreeting(greeting);
-			} catch (error) {
-				console.log(error);
-			}
-		}
-		getGreeting();
+		async function getPokemonList() {
+			const pokemonList = await (await fetch('/api/getpokemonlist')).json();
+			dispatch({ type: "SET_POKEMON_LIST", payload: pokemonList });
+		};
+		getPokemonList()
 	}, []);
-
-  useEffect(() => {
-    async function getPokemonList() {
-		const pokemonList = await (await fetch('/api/getpokemonlist')).json();
-		console.log(pokemonList)
-		dispatch({ type: "SET_POKEMON_LIST", payload: pokemonList });
-	};
-	getPokemonList()
-  }, []);
 
 
 	return (
